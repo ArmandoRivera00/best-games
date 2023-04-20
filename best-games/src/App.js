d3c1d59b3584172585dcd387d774efd38138e4a8
Armando <107088320+ArmandoRivera00@users.noreply.github.com>
2023-04-19 17:19:13 -0700

    Add files via upload

---
+++ best-games/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2>Best game's of all time! according to Armando</h2>
+      <h3>This is a list of Armando's favourite games of all time.</h3>
+      <ul>
+        <li> Grand Theft Auto IV</li>
+        <li>Portal (2007)</li>
+        <li>Fortnite, Battle Royale</li>
+        <li>Spider-Man: Miles Morales</li>
+        <li>Descenders</li>
+      </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
