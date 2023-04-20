47ca45bc1620f0ad20dc38d22e85992e5c7846b7
Armando <107088320+ArmandoRivera00@users.noreply.github.com>
2023-04-20 15:18:34 -0700

    Add files via upload

--- best-games/src/App.js
+++ best-games/src/App.js
@@ -7,14 +7,44 @@ function App() {
       <header className="App-header">
       <h2>Best game's of all time! according to Armando</h2>
       <h3>This is a list of Armando's favourite games of all time.</h3>
-      <ul>
+      <section>
+        <ul>
         <li> Grand Theft Auto IV</li>
         <li>Portal (2007)</li>
-        <li>Fortnite, Battle Royale</li>
+        <li>Fortnite: Battle Royale</li>
         <li>Spider-Man: Miles Morales</li>
         <li>Descenders</li>
       </ul>
+      </section>
       </header>
+      <section1>
+        <h1>
+          Grand Theft Auto IV
+        </h1>
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/M80K51DosFo" title="Grand Theft Auto IV Trailer 1 &quot;Things Will Be Different&quot;" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <p>
+          Grand Theft Auto IV is a 2008 game developed and published by Rockstar Games. 
+        </p>
+        <p1>
+        This game was initially available for the Xbox 360 and PS3.
+        The reason why GTA IV made it on my personal list is because I played it and really enjoyed how the story of it developed and the overall gameplay compared
+        to other games on the Grand Theft Auto Saga.
+        </p1>
+        </section1>
+      <section2>
+          <h1>
+            Portal (2007)
+          </h1>
+          <iframe width="1280" height="720" src="https://www.youtube.com/embed/zQhCTRHYBZQ" title="Portal 1 Trailer Oficial [HD]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <p3>
+            Portal is a game developed and published by Valve inc in 2007, for the PC platform
+            as well as the Xbox 360 and PS3 as a part of "The Orange Box Bundle"
+          </p3>
+          <p4>
+            I got introduced to this game not too long ago, when  I bought it during the Easter steam sale on the Valve Complete Pack.
+            Afterwards I asked friends what I should stream, and a lot of them suggested me Portal! Which 
+          </p4>
+        </section2>
     </div>
   );
 }
