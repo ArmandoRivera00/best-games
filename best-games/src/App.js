edca26287b9024fd6810cb9d77755b414397bce0
Armando <107088320+ArmandoRivera00@users.noreply.github.com>
2023-04-21 13:42:28 -0700

    Add files via upload

--- best-games/src/App.js
+++ best-games/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h2>Best game's of all time! according to Armando</h2>
+      <h2>Best games of all time! according to Armando</h2>
       <h3>This is a list of Armando's favourite games of all time.</h3>
       <section>
         <ul>
@@ -50,6 +50,11 @@ function App() {
             Afterwards I asked friends what I should stream, and a lot of them suggested me Portal! Which I ended up enjoying and
             ended up appreciating this game entirely.
           </p4>
+          <h1>
+            Screenshots:
+          </h1>
+          <img src='https://cdn.nivoli.com/adventuregamers/images/screenshots/16495/8411.jpg'></img>
+          <img src='https://cdn.mobygames.com/screenshots/11303673-portal-windows-meet-glados-you-dont-like-her-her-feelings-are-mu.jpg'></img>
         </section2>
         <section3>
           <h1>
@@ -81,12 +86,23 @@ function App() {
           <p8> This game made it on my list due to the amazing story expanding from the first game, and going straight to the point.
             Even though this game was way shorter than the original one "Marvel's Spider-Man" I found this being a more compelling 
             and polished game. </p8>
+            <h1>
+              Screenshots:
+            </h1>
+            <img src='https://sportshub.cbsistatic.com/i/2021/03/18/8cb4b7c4-25a1-4205-9af9-2a329ed08559/spider-man-miles-morales-1224324.jpg'></img>
+            <img src='https://press-start.com.au/news/playstation/2020/10/14/heres-new-spider-man-miles-morales-ps5-screenshots-and-details/attachment/eko-wnqxyaevvfz/'></img>
         </section4>
         <section5>
           <h1>Descenders</h1>
           <iframe width="1920" height="1080" src="https://www.youtube.com/embed/7AqS1YdN6n8" title="Descenders Reveal Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <p9>Descenders is a 2018 videogame released for the PC with subsequent releases on Xbox, Android, and Nintendo Switch.</p9>
-          <p10>Honestly, I found this game around 2021, </p10>
+          <p9>Descenders is a 2018 videogame released for the PC with subsequent releases on Xbox, Android, and Nintendo Switch which is about free bikeriding.</p9>
+          <p10>Honestly, I found this game around 2021, and tried it for the first time and I fell in love with it. It was a breath of fresh air for me as I had never experimented
+            this type of game and it just stuck with me and I loved it. </p10>
+          <h1>
+            Screenshots:
+          </h1>
+          <img src='https://www.gamespace.com/wp-content/uploads/2018/02/2018.02.11-22.10.png'></img>
+          <img src='https://images.nintendolife.com/screenshots/110071/large.jpg'></img>
         </section5>
     </div>
   );
