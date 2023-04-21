5bc085ca849ab8b70f46524cabd2b461793d057d
Armando <107088320+ArmandoRivera00@users.noreply.github.com>
2023-04-21 13:32:19 -0700

    Add files via upload

--- best-games/src/App.js
+++ best-games/src/App.js
@@ -21,7 +21,7 @@ function App() {
         <h1>
           Grand Theft Auto IV
         </h1>
-        <iframe width="1280" height="720" src="https://www.youtube.com/embed/M80K51DosFo" title="Grand Theft Auto IV Trailer 1 &quot;Things Will Be Different&quot;" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="1920" height="1080" src="https://www.youtube.com/embed/M80K51DosFo" title="Grand Theft Auto IV Trailer 1 &quot;Things Will Be Different&quot;" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <p>
           Grand Theft Auto IV is a 2008 game developed and published by Rockstar Games. 
         </p>
@@ -30,21 +30,64 @@ function App() {
         The reason why GTA IV made it on my personal list is because I played it and really enjoyed how the story of it developed and the overall gameplay compared
         to other games on the Grand Theft Auto Saga.
         </p1>
+        <h1>
+          Screenshots:
+        </h1>
+        <img src='https://media.gtanet.com/images/4803-gta-iv-screenshot.jpg'></img>
+        <img src='https://www.newgamenetwork.com/images/uploads/gallery/GTA4/gtaiv-20081213-190841.png'></img>
         </section1>
       <section2>
           <h1>
             Portal (2007)
           </h1>
-          <iframe width="1280" height="720" src="https://www.youtube.com/embed/zQhCTRHYBZQ" title="Portal 1 Trailer Oficial [HD]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe width="1920" height="1080" src="https://www.youtube.com/embed/zQhCTRHYBZQ" title="Portal 1 Trailer Oficial [HD]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           <p3>
             Portal is a game developed and published by Valve inc in 2007, for the PC platform
             as well as the Xbox 360 and PS3 as a part of "The Orange Box Bundle"
           </p3>
           <p4>
             I got introduced to this game not too long ago, when  I bought it during the Easter steam sale on the Valve Complete Pack.
-            Afterwards I asked friends what I should stream, and a lot of them suggested me Portal! Which 
+            Afterwards I asked friends what I should stream, and a lot of them suggested me Portal! Which I ended up enjoying and
+            ended up appreciating this game entirely.
           </p4>
         </section2>
+        <section3>
+          <h1>
+            Fortnite: Battle Royale
+          </h1>
+          <iframe width="1920" height="1080" src="https://www.youtube.com/embed/66t5L6EhPMM" title="Fortnite Battle Royale - Gameplay Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <p5>
+            Fortnite Battle Royale is a 2017 game developed and published by Epic Games for the Xbox One, PS4, and PC. Focusing on a new to the time style called Battle Royale
+            where you are sent on an island and fight your way to the top against anyone that gets in your way.
+          </p5>
+          <p6>
+            I got into this game back in 2019 and started playing on the Nintendo Switch where after a while I got a lot into it and honestly, with a lot of updates
+            I've grown to love this simple yet complex game as I play with friends or by myself.
+          </p6>
+          <h1>
+            Screenshots:
+          </h1>
+          <img src='https://cdn2.unrealengine.com/Fortnite%2Fblog%2Fphotography-blog%2F19_1011_Upgraded_Combat_Screenshot-1920x1080-b7fb7387b1402312bbbe8fbba6954e1715095059.png'></img>
+          <img src='https://cdn2.unrealengine.com/Fortnite%2Fblog%2Fphotography-blog%2Fpic3-1600x900-847cbb3d261ae1314f0f94145d4ef4321530d6c0.png'></img>
+        </section3>
+        <section4>
+          <h1>
+            Spider-Man: Miles Morales
+          </h1>
+          <iframe width="1920" height="1080" src="https://www.youtube.com/embed/NTunTURbyUU" title="Marvel’s Spider-Man: Miles Morales Launch Trailer I PS5, PS4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <p7>
+            Spider-Man: Miles Morales is a 2020 game released originally for the Play Station 5 and recently released on PC.
+          </p7>
+          <p8> This game made it on my list due to the amazing story expanding from the first game, and going straight to the point.
+            Even though this game was way shorter than the original one "Marvel's Spider-Man" I found this being a more compelling 
+            and polished game. </p8>
+        </section4>
+        <section5>
+          <h1>Descenders</h1>
+          <iframe width="1920" height="1080" src="https://www.youtube.com/embed/7AqS1YdN6n8" title="Descenders Reveal Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <p9>Descenders is a 2018 videogame released for the PC with subsequent releases on Xbox, Android, and Nintendo Switch.</p9>
+          <p10>Honestly, I found this game around 2021, </p10>
+        </section5>
     </div>
   );
 }
